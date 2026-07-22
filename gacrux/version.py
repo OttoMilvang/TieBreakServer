e0526f469bd92cb67fe58bf30ce5c6d4ed520bc9
otto <otto@milvang.no>
2026-07-22 23:11:07 +0200

    Moved py files to gacrux dir

--- gacrux/version.py
+++ gacrux/version.py
@@ -5,15 +5,15 @@ Created on Tue Apr 29 09:21:45 2025
 @author: otto
 """
 
-__version__ = "1.9.57"
+__version__ = "1.9.58"
 
 def version():
     return {
         "version": __version__,
         "version_major": 1,
         "version_minor": 9,
-        "version_patch": 57,
-        "version_date": "2026-07-21",
+        "version_patch": 58,
+        "version_date": "2026-07-22",
        }
 
 if __name__ == "__main__":
